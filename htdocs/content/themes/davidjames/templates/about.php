9c65703b46898211619871d6b53e9a52606f59e5
Drew Roberts <drew.roberts@methodfactory.com>
2015-11-05 11:15:42 -0500

    about hero / content

--- htdocs/content/themes/davidjames/templates/about.php
+++ htdocs/content/themes/davidjames/templates/about.php
@@ -11,84 +11,59 @@
 get_header();
 ?>
 <?php include (TEMPLATEPATH . '/nav-main.php'); ?>
-<div class="jumbotron" role="hero">
-  <div class="container">
-    <div class="hero__logo">
-      <img src="<?php bloginfo(template_url);?>/img/david-james-signature-light.png" alt="David James Logo" />
-    </div>
-    <div class="hero__content">
-      <p>About Page</p>
-      <a class="jumbotron__link link--underline" href="#" role="cta">Watch Trailer</a>
-    </div>
-  </div>
-</div>
-<div class="block new-book--wrapper container">
+<div class="jumbotron" role="hero"></div>
+<div class="block about--wrapper container">
   <div class="row">
     <div class="about--wrapper col-xs-12 col-sm-4">
-      <h4 class="about-heading--secondary">new book from david james:</h4>
-      <h2 class="about-heading--primary">Steps</h2>
+      <h4 class="about-heading--secondary">biography</h4>
+      <h2 class="about-heading--primary">about david james</h2>
     </div>
     <div class="about-content col-xs-12 col-sm-8">
       <p class="about-content__paragraph">
-        On sale now, Steps is a story of growth, reinvention, death and love, reminding us that there is always hope and everyday is a new opportunity to become better. It is the memoir of Issac, a country boy, who, after becoming unsatisfied with life in West Texas, travels the world in search of love and meaning. At every turn, however, he is met with insurmountable obstacles. Drawing on his relentless, dogged refusal to give up, he steadfastly confronts his demons. But, will it be enough to overcome the impossible odds against him?
+        David James is the author of Steps, his debut novel.
       </p>
-      <div class="about__stats--wrapper">
-        <div class="row">
-          <div class="about__stat col-xs-3">
-            <span class="about__stat-number">407</span>
-            <span class="about__stat-label">pages</span>
-          </div>
-          <div class="about__stat col-xs-3">
-            <span class="about__stat-number">54</span>
-            <span class="about__stat-label">paintings</span>
-          </div>
-          <div class="about__stat col-xs-3">
-            <span class="about__stat-number">6</span>
-            <span class="about__stat-label">prose</span>
-          </div>
-        </div>
-      </div>
+      <p class="about-content__paragraph">
+        Born to a country family in the spring of 1950, James spent most of his foundational years as a ranchhand in west Texas. While growing up as a cowboy, he developed a unique passion for art and literature. A lifelong painter, James has worked professionally as an artist for over forty years.
+      </p>
+      <p class="about-content__paragraph">
+        Unsatisfied with life in the country, James left west Texas to travel and live throughout the world — mostly out of a backpack. Having considered himself fortunate for his experiences, he now sees himself as a communicator and teacher rather than an artist or writer. Although he is certainly also those things.
+      </p>
+      <p class="about-content__paragraph">
+        With no formal education, James’ artistic training stems from a deep appreciation of the works of Van Gogh, Gauguin and Cezanne, as well as the literature of D.H. Lawrence, Gertrude Stein and John Steinbeck, among others.
+      </p>
+      <p class="about-content__paragraph">
+        James’ debut novel, Steps, tells a story of redemption and new beginnings — something he has been forced to experience more than once.
+      </p>
+      <p class="about-content__paragraph">
+        Today, James lives in Austin, Texas with his wife and son.
+      </p>
+      <p class="about-content__paragraph">
+        Today, James lives in Austin, Texas with his wife and son.
+      </p>
+      <p class="about-content__paragraph">
+        If you’re interested in reading some of his work online, you can download selected excerpts from Steps here, and you can watch the book trailer of “The Killings,” an excerpt from Steps, here.
+      </p>
     </div>
   </div>
 </div>
-<div class="block home-art--wrapper container-fluid">
+<div class="block two-col-feature container">
   <div class="row">
-    <div class="home-art__artwork--wrapper col-md-6 col--full-width">
-      <div class="home-art__artwork-row">
-        <div class="home-art__artwork-content">
-          <a class="home-art__link" href="#">view the david james art gallery</a>
-        </div>
-        <img class="home-art__artwork-image" src="<?php bloginfo('template_url'); ?>/img/art-01.jpg" alt="artwork 01" />
-      </div>
-    </div>
-    <div class="home-art__artwork--wrapper col-md-6 col--full-width">
-      <div class="home-art__artwork--row">
-        <img class="home-art__artwork-image" src="<?php bloginfo('template_url'); ?>/img/art-02.jpg" alt="artwork 02" />
-      </div>
-      <div class="home-art__artwork--row">
-        <img class="home-art__artwork-image" src="<?php bloginfo('template_url'); ?>/img/art-03.jpg" alt="artwork 03" />
-      </div>
-    </div>
-  </div>
-</div>
-<div class="block home-blog--wrapper container">
-  <div class="row">
-    <div class="home-blog__posting home-blog__rule col-md-6">
-      <div class="home-blog__heading">
+    <div class="two-col-feature__col two-col-feature__rule col-md-6">
+      <div class="two-col-feature__heading">
         <h2>This is the Title of the Latest Blog Post</h2>
         <h5 class="date">12-10-15</h5>
       </div>
-      <div class="home-blog__content">
+      <div class="two-col-feature__content">
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
       </div>
       <a class="link--read-more" href="#">read more</a>
     </div>
-    <div class="home-blog__posting col-md-6">
-      <div class="home-blog__heading">
+    <div class="two-col-feature__col col-md-6">
+      <div class="two-col-feature__heading">
         <h2>This is the Title of the Latest Blog Post</h2>
         <span class="date">12-10-15</span>
       </div>
-      <div class="home-blog__content">
+      <div class="two-col-feature__content">
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
       </div>
       <a class="link--read-more" href="#">read more</a>
