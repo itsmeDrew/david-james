c9709349d9248fc394dcd59056167f8256dd75b1
Drew Roberts <drew.roberts@methodfactory.com>
2015-11-06 14:06:27 -0500

    art page styling / woocommerce

--- htdocs/content/themes/davidjames/woocommerce/archive-product.php
+++ htdocs/content/themes/davidjames/woocommerce/archive-product.php
@@ -4,98 +4,42 @@
  *
  * Override this template by copying it to yourtheme/woocommerce/archive-product.php
  *
- * @author 		WooThemes
- * @package 	WooCommerce/Templates
- * @version     2.0.0
+ * @package David James
+ * @subpackage david-james
+ * @since David James 1.0.1
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
 }
 
-get_header( 'shop' ); ?>
-<h2>TESTINGS</h2>
-	<?php
-		/**
-		 * woocommerce_before_main_content hook
-		 *
-		 * @hooked woocommerce_output_content_wrapper - 10 (outputs opening divs for the content)
-		 * @hooked woocommerce_breadcrumb - 20
-		 */
-		do_action( 'woocommerce_before_main_content' );
-	?>
+get_header(); ?>
+<?php include (TEMPLATEPATH . '/nav-main.php'); ?>
+<?php if ( have_posts() ) : ?>
 
-		<?php if ( apply_filters( 'woocommerce_show_page_title', true ) ) : ?>
+<div class="block product-listing--wrapper container-fluid">
+	<header class="product-listing__header">
+		<h2 class="product-listing__heading">The Artwork of David James</h2>
+	</header>
+	<div class="product-listing__facets row">
+		<?php do_action( 'woocommerce_before_shop_loop' ); ?>
+		<?php do_action( 'woocommerce_sidebar' ); ?>
+	</div>
+	<?php woocommerce_product_loop_start(); ?>
 
-			<h1 class="page-title"><?php woocommerce_page_title(); ?></h1>
+	<?php while ( have_posts() ) : the_post(); ?>
+		<?php wc_get_template_part( 'content', 'product' ); ?>
+	<?php endwhile; // end of the loop. ?>
 
-		<?php endif; ?>
+	<?php woocommerce_product_loop_end(); ?>
 
-		<?php
-			/**
-			 * woocommerce_archive_description hook
-			 *
-			 * @hooked woocommerce_taxonomy_archive_description - 10
-			 * @hooked woocommerce_product_archive_description - 10
-			 */
-			do_action( 'woocommerce_archive_description' );
-		?>
+	<?php do_action( 'woocommerce_after_shop_loop' ); ?>
 
-		<?php if ( have_posts() ) : ?>
+	<?php elseif ( ! woocommerce_product_subcategories( array( 'before' => woocommerce_product_loop_start( false ), 'after' => woocommerce_product_loop_end( false ) ) ) ) : ?>
+		<?php wc_get_template( 'loop/no-products-found.php' ); ?>
+	<?php endif; ?>
 
-			<?php
-				/**
-				 * woocommerce_before_shop_loop hook
-				 *
-				 * @hooked woocommerce_result_count - 20
-				 * @hooked woocommerce_catalog_ordering - 30
-				 */
-				do_action( 'woocommerce_before_shop_loop' );
-			?>
-
-			<?php woocommerce_product_loop_start(); ?>
-
-				<?php woocommerce_product_subcategories(); ?>
-
-				<?php while ( have_posts() ) : the_post(); ?>
-
-					<?php wc_get_template_part( 'content', 'product' ); ?>
-
-				<?php endwhile; // end of the loop. ?>
-
-			<?php woocommerce_product_loop_end(); ?>
-
-			<?php
-				/**
-				 * woocommerce_after_shop_loop hook
-				 *
-				 * @hooked woocommerce_pagination - 10
-				 */
-				do_action( 'woocommerce_after_shop_loop' );
-			?>
-
-		<?php elseif ( ! woocommerce_product_subcategories( array( 'before' => woocommerce_product_loop_start( false ), 'after' => woocommerce_product_loop_end( false ) ) ) ) : ?>
-
-			<?php wc_get_template( 'loop/no-products-found.php' ); ?>
-
-		<?php endif; ?>
-
-	<?php
-		/**
-		 * woocommerce_after_main_content hook
-		 *
-		 * @hooked woocommerce_output_content_wrapper_end - 10 (outputs closing divs for the content)
-		 */
-		do_action( 'woocommerce_after_main_content' );
-	?>
-
-	<?php
-		/**
-		 * woocommerce_sidebar hook
-		 *
-		 * @hooked woocommerce_get_sidebar - 10
-		 */
-		do_action( 'woocommerce_sidebar' );
-	?>
-
-<?php get_footer( 'shop' ); ?>
+	<?php do_action( 'woocommerce_after_main_content' ); ?>
+	
+</div>
+<?php get_footer(); ?>
