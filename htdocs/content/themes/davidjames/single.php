0f7143cfac4041de3bff309026780b72c2c7403b
Drew Roberts <drew.roberts@methodfactory.com>
2015-11-06 11:17:35 -0500

    single page styling

--- htdocs/content/themes/davidjames/single.php
+++ htdocs/content/themes/davidjames/single.php
@@ -8,44 +8,33 @@
  */
 
 get_header(); ?>
+<?php include (TEMPLATEPATH . '/nav-main.php'); ?>
+
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
 <?php $thumbnail = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), array(1200, 350) ); ?>
 <?php if ($thumbnail[0]) : ?>
 
-<div class="blurred-container" style="background-image: url('<?php echo $thumbnail[0]; ?>')">
-  <div id="navbar-full">
-    <div id="navbar">
-      <?php include (TEMPLATEPATH . '/nav-main.php'); ?>
-      <div class="container">
-        <div class="hero-container">
-            <div class="motto">
-              <h3><?php the_title(); ?></h3>
-              <p>Posted on <?php the_time('D, M, Y'); ?></p>
-            </div>
-        </div>
-      </div>
-		</div>
-	</div>
-	<div class="blurred-container__overlay"></div>
-</div><!--  end navbar -->
-<?php endif; ?>
-<div id="one" class="main">
-	<div class="col-md-8 col-md-offset-2 col-sm-10 col-sm-offset-1">
-		<article class="blog-posting__content">
-			<?php the_content(); ?>
-			<?php endwhile; endif; wp_reset_postdata(); ?>
-		</article>
-	</div>
-	<div class="col-md-8 col-md-offset-2 col-sm-10 col-sm-offset-1">
-		<div class="blog-posting__comments--wrapper">
-			<?php
-			    if ( comments_open() || get_comments_number() ) :
-			      comments_template();
-			    endif;
-			?>
-		</div>
-	</div>
+<div class="jumbotron js-sticky-scroll" role="hero" style="background-image: url('<?php echo $thumbnail[0]; ?>')">
+  <div class="container">
+    <div class="hero__content">
+      <h4 class="about-heading--secondary">written by <?php the_author(); ?> | <?php the_time('F j, Y'); ?></h4>
+      <h2 class="about-heading--primary"><a href="<?php echo the_permalink();?>"><?php the_title(); ?></a></h2>
+    </div>
+  </div>
+  <div class="jumbotron__overlay"></div>
 </div>
+<div class="block container">
+  <div class="row">
+    <div class="col-sm-12">
+      <?php the_content(); ?>
+    </div>
+  </div>
+</div>
+
+<?php endif; ?>
+<?php endwhile; endif; wp_reset_postdata(); ?>
+
+
 
 <?php get_footer(); ?>
